b637415f7b4d67af03c2a7fbe0d816e9e1fd8c8d
Manabu Tokunaga <manabu@imorgon.net>
2012-03-09 16:43:48 -0800

    First Commit

---
+++ RegExodus/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace RegExplore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxRegEx = new System.Windows.Forms.TextBox();
+            this.textBoxSample = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxTemplates = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxMatches = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonMatch = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelMatchesCount = new System.Windows.Forms.Label();
+            this.textBoxReplaceText = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Regular Expression";
+            // 
+            // textBoxRegEx
+            // 
+            this.textBoxRegEx.Location = new System.Drawing.Point(37, 49);
+            this.textBoxRegEx.Multiline = true;
+            this.textBoxRegEx.Name = "textBoxRegEx";
+            this.textBoxRegEx.Size = new System.Drawing.Size(638, 20);
+            this.textBoxRegEx.TabIndex = 1;
+            // 
+            // textBoxSample
+            // 
+            this.textBoxSample.Location = new System.Drawing.Point(37, 94);
+            this.textBoxSample.Multiline = true;
+            this.textBoxSample.Name = "textBoxSample";
+            this.textBoxSample.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxSample.Size = new System.Drawing.Size(638, 69);
+            this.textBoxSample.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sample Text";
+            // 
+            // listBoxTemplates
+            // 
+            this.listBoxTemplates.DisplayMember = "value";
+            this.listBoxTemplates.FormattingEnabled = true;
+            this.listBoxTemplates.Location = new System.Drawing.Point(698, 49);
+            this.listBoxTemplates.Name = "listBoxTemplates";
+            this.listBoxTemplates.Size = new System.Drawing.Size(221, 420);
+            this.listBoxTemplates.TabIndex = 4;
+            this.listBoxTemplates.ValueMember = "key";
+            this.listBoxTemplates.SelectedIndexChanged += new System.EventHandler(this.listBoxTemplates_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 492);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(945, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(695, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Template";
+            // 
+            // listBoxMatches
+            // 
+            this.listBoxMatches.FormattingEnabled = true;
+            this.listBoxMatches.Location = new System.Drawing.Point(37, 236);
+            this.listBoxMatches.Name = "listBoxMatches";
+            this.listBoxMatches.Size = new System.Drawing.Size(345, 186);
+            this.listBoxMatches.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Matches";
+            // 
+            // buttonMatch
+            // 
+            this.buttonMatch.Location = new System.Drawing.Point(600, 186);
+            this.buttonMatch.Name = "buttonMatch";
+            this.buttonMatch.Size = new System.Drawing.Size(75, 23);
+            this.buttonMatch.TabIndex = 9;
+            this.buttonMatch.Text = "Match";
+            this.buttonMatch.UseVisualStyleBackColor = true;
+            this.buttonMatch.Click += new System.EventHandler(this.buttonMatch_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(404, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Matches";
+            // 
+            // labelMatchesCount
+            // 
+            this.labelMatchesCount.AutoSize = true;
+            this.labelMatchesCount.Location = new System.Drawing.Point(473, 236);
+            this.labelMatchesCount.Name = "labelMatchesCount";
+            this.labelMatchesCount.Size = new System.Drawing.Size(13, 13);
+            this.labelMatchesCount.TabIndex = 11;
+            this.labelMatchesCount.Text = "0";
+            // 
+            // textBoxReplaceText
+            // 
+            this.textBoxReplaceText.Location = new System.Drawing.Point(37, 186);
+            this.textBoxReplaceText.Multiline = true;
+            this.textBoxReplaceText.Name = "textBoxReplaceText";
+            this.textBoxReplaceText.Size = new System.Drawing.Size(449, 20);
+            this.textBoxReplaceText.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Replace Text";
+            // 
+            // buttonReplace
+            // 
+            this.buttonReplace.Location = new System.Drawing.Point(509, 186);
+            this.buttonReplace.Name = "buttonReplace";
+            this.buttonReplace.Size = new System.Drawing.Size(75, 23);
+            this.buttonReplace.TabIndex = 14;
+            this.buttonReplace.Text = "Replace";
+            this.buttonReplace.UseVisualStyleBackColor = true;
+            this.buttonReplace.Click += new System.EventHandler(this.buttonReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 514);
+            this.Controls.Add(this.buttonReplace);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxReplaceText);
+            this.Controls.Add(this.labelMatchesCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonMatch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBoxMatches);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBoxTemplates);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSample);
+            this.Controls.Add(this.textBoxRegEx);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RegularExodus 360:  NET Framework Regular Expression Tester ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxRegEx;
+        private System.Windows.Forms.TextBox textBoxSample;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxTemplates;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxMatches;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonMatch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelMatchesCount;
+        private System.Windows.Forms.TextBox textBoxReplaceText;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonReplace;
+    }
+}
+
---
+++ RegExodus/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+using System.Xml.Linq;
+using System.Linq;
+
+namespace RegExplore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxRegEx.Text = Properties.Settings.Default.RegEx;
+            textBoxSample.Text = Properties.Settings.Default.Sample;
+            FillTemplate();
+        }
+
+        private void buttonMatch_Click(object sender, EventArgs e)
+        {
+            var re = new Regex(textBoxRegEx.Text);
+            var ms = re.Matches(textBoxSample.Text);
+            labelMatchesCount.Text = ms.Count.ToString();
+
+            Properties.Settings.Default.RegEx = textBoxRegEx.Text;
+            Properties.Settings.Default.Sample = textBoxSample.Text;
+            Properties.Settings.Default.Save();
+
+            listBoxMatches.Items.Clear();
+
+            foreach (var m in ms)
+            {
+                listBoxMatches.Items.Add(m.ToString());
+            }
+        }
+
+        private void buttonReplace_Click(object sender, EventArgs e)
+        {
+            var re = new Regex(textBoxRegEx.Text);
+            var ms = re.Matches(textBoxSample.Text);
+            labelMatchesCount.Text = ms.Count.ToString();
+
+            Properties.Settings.Default.RegEx = textBoxRegEx.Text;
+            Properties.Settings.Default.Sample = textBoxSample.Text;
+            Properties.Settings.Default.Save();
+
+            var rep = re.Replace(textBoxSample.Text, textBoxReplaceText.Text);
+
+            listBoxMatches.Items.Clear();
+            listBoxMatches.Items.Add(rep);
+        }
+
+        Dictionary<string,string> items = new Dictionary<string, string>();
+
+        private void FillTemplate()
+        {
+            var xd = XDocument.Load("RegExTemplate.xml");
+
+            var xr = from x in xd.Elements("template")
+                 select x;
+
+
+            foreach (var y in xr.Elements())
+            {
+                var d = y.Attribute("d").Value;
+                var e = y.Attribute("ex").Value;
+                items.Add(e, d);
+            }
+
+            foreach (var i in items)
+            {
+       
+                listBoxTemplates.Items.Add(i);
+            }
+
+            
+            
+        }
+
+        private void listBoxTemplates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var i = (KeyValuePair<string,string>)listBoxTemplates.SelectedItem;
+            textBoxRegEx.Text = textBoxRegEx.Text + i.Key;
+            
+        }
+    }
+}
